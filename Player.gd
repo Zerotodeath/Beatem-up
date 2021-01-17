81464570c9dfd5ea5a0e1d6c065e2ce2ecc130e0
Zerotodeath <41883901+Zerotodeath@users.noreply.github.com>
2021-01-17 02:48:29 -0500

    idk

--- Player.gd
+++ Player.gd
@@ -8,21 +8,14 @@ enum{
 const ACCEL = 600
 const MAXSPEED = 400
 
-var state = Moving
+var state = Attacking
 
 var Motion = Vector2.ZERO
 var Direction = 1
 
 signal Attacked
 
-func _process(delta):
-	match state:
-		Moving:
-			Moving(delta)
-		Attacking:
-			Attack()
-
-func Moving(delta):
+func _physics_process(delta):
 	var Xinput = Input.get_action_strength("Input_right") - Input.get_action_strength("Input_left")
 	var Yinput = Input.get_action_strength("Input_down") - Input.get_action_strength("Input_up")
 	
@@ -37,16 +30,13 @@ func Moving(delta):
 	else:
 		Motion.y = 0
 	
-	$AnimationPlayer.play("Idle")
-	$Hitbox/CollisionShape2D.disabled = true
 	
 	if Input.is_action_just_pressed("Input_attack"):
-		Attack()
+		emit_signal("Attacked")
 		$AnimationPlayer.play("New Anim")
+	else:
+		$AnimationPlayer.play("Idle")
 	
 	move_and_slide(Motion)
-
-func Attack():
-	emit_signal("Attacked")
 
 
