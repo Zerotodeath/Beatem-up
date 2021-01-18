c7af7917c4c6cb08547a159de4d2a320c3e6423c
Zerotodeath <41883901+Zerotodeath@users.noreply.github.com>
2021-01-18 16:53:05 -0500

    Improve code

--- Player.gd
+++ Player.gd
@@ -12,12 +12,17 @@ const MAXSPEED = 400
 
 var state = Moving
 
+var Xinput
+var Yinput
+
 var Motion = Vector2.ZERO
 var Direction = 1
 
 signal Attacked
 
 func _process(delta):
+	print(Xinput)
+	
 	match state:
 		Moving:
 			Move(delta)
@@ -27,15 +32,16 @@ func _process(delta):
 			Idling(delta)
 
 func Move(delta):
-	var Xinput = Input.get_action_strength("Input_right") - Input.get_action_strength("Input_left")
-	var Yinput = Input.get_action_strength("Input_down") - Input.get_action_strength("Input_up")
+	Xinput = Input.get_action_strength("Input_right") - Input.get_action_strength("Input_left")
+	Yinput = Input.get_action_strength("Input_down") - Input.get_action_strength("Input_up")
 	
 	if Xinput != 0:
 		Motion.x += Xinput * ACCEL * delta
 		Motion.x = clamp(Motion.x, -MAXSPEED, MAXSPEED)
 		$AnimationPlayer.play("Run")
-	else:
-		Motion.x = lerp(Motion.x, 0, 0.22)
+	
+	if Xinput == 0:
+		state = Idle
 	
 	if Yinput != 0:
 		Motion.y = 250 * Yinput
@@ -52,7 +58,7 @@ func Move(delta):
 func Idling(delta):
 	$AnimationPlayer.play("Idle")
 	
-	Motion.x = 0
+	Motion.x = lerp(Motion.x, 0, 0.22)
 	
 	if Input.is_action_just_pressed("Input_right") || Input.is_action_just_pressed("Input_left"):
 		state = Moving
