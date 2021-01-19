f314589814788e46660e558a357c980286b1b4c3
Zerotodeath <41883901+Zerotodeath@users.noreply.github.com>
2021-01-18 22:09:05 -0500

    Fix attacking

--- Player.gd
+++ Player.gd
@@ -12,16 +12,15 @@ const MAXSPEED = 400
 
 var state = Moving
 
-var Xinput
-var Yinput
-
 var Motion = Vector2.ZERO
 var Direction = 1
+
+var Xinput
+var Yinput
 
 signal Attacked
 
 func _process(delta):
-	
 	match state:
 		Moving:
 			Move(delta)
@@ -38,17 +37,13 @@ func Move(delta):
 		Motion.x += Xinput * ACCEL * delta
 		Motion.x = clamp(Motion.x, -MAXSPEED, MAXSPEED)
 		$AnimationPlayer.play("Run")
+	if Yinput != 0:
+		Motion.y += 550 * Yinput * delta
+		Motion.y = clamp(Motion.y, -300, 300)
 	
 	if Xinput == 0:
-		state = Idle
-	
-	if Yinput != 0:
-		Motion.y = 250 * Yinput
-	else:
-		Motion.y = 0
-	
-	if Motion.x == 0:
-		state = Idle
+		if Yinput == 0:
+			state = Idle
 	
 	if Input.is_action_just_pressed("Input_attack"):
 		state = Attacking
@@ -56,26 +51,29 @@ func Move(delta):
 
 func Idling(delta):
 	$AnimationPlayer.play("Idle")
+	$Hitbox/CollisionShape2D.disabled = true
 	
 	Motion.x = lerp(Motion.x, 0, 0.22)
+	Motion.y = lerp(Motion.y, 0, .27)
 	
-	if Input.is_action_just_pressed("Input_right") || Input.is_action_just_pressed("Input_left"):
+	if Input.is_action_pressed("Input_right") || Input.is_action_just_pressed("Input_left") || Input.is_action_just_pressed("Input_up") || Input.is_action_just_pressed("Input_down"):
 		state = Moving
-	if Input.is_action_just_pressed("Input_attack"):
+	if Input.is_action_pressed("Input_attack"):
 		state = Attacking
 	move_and_slide(Motion)
 
 func Attack(delta):
 	$AnimationPlayer.play("Attack")
-	Motion.x = 0
-	Motion.y = 0
+	Motion.x = lerp(Motion.x, 0, .1)
+	Motion.y = lerp(Motion.y, 0, .12)
 	
-	if Input.is_action_just_pressed("Input_right") || Input.is_action_just_pressed("Input_left"):
-		state = Moving
+	if Xinput != 0:
+		if Yinput != 0:
+			state = Moving
 	
 	move_and_slide(Motion)
 
 
 func _on_AnimationPlayer_animation_finished(Attack):
 	if state == Attacking:
-		state = Idle
+		state = Moving
